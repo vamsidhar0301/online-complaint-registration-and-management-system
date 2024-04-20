353745252797fbec246b4296417e0d2f4ff0ff08
vamsidhar0301 <167609534+vamsidhar0301@users.noreply.github.com>
2024-04-20 14:11:34 +0530

    Create db.js

---
+++ db.js
@@ -0,0 +1,9 @@
+
+const mongoose = require('mongoose')
+
+function connect () {
+    mongoose.set('useCreateIndex', true);
+    mongoose.connect('mongodb://localhost/complaintapp',{useNewUrlParser: true})
+}
+
+module.exports = connect
